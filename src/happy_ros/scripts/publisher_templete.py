20fba25c537b44edfca19e51caf515f211739369
seunggeon <tmdrjs9555@naver.com>
2022-03-03 12:39:30 +0900

    polygon publish exercise

--- src/happy_ros/scripts/publisher_templete.py
+++ src/happy_ros/scripts/publisher_templete.py
@@ -2,19 +2,21 @@
 
 #-*- coding:utf-8 -*-
 
-from ast import If
+from msilib.schema import PublishComponent
 import rospy 
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PolygonStamped
+from geometry_msgs.msg import Point32
+from visualization_msgs.msg import Marker
 
-NODE_NAME = 'turtle_mover'
+NODE_NAME = 'publish_node'
 
 TOPIC_NAME = 'cmd_vel'
 
-MSG_TYPE = Twist
+MSG_TYPE = PolygonStamped
 
 PUBLISH_HZ = 10.0
 
-class turtle_mover() : 
+class topic_publisher() : 
 
     def __init__(self) : 
 
@@ -23,10 +25,29 @@ class turtle_mover() :
         self.publisher1 = rospy.Publisher(TOPIC_NAME , MSG_TYPE, queue_size =1 )
 
         rospy.Timer(rospy.Duration(1.0 / PUBLISH_HZ), self.timer_CB)
+        rospy.spin()
 
     def timer_CB(self, event) :
+        publishing_data = PolygonStamped()
+        
+        publishing_data.header.frame_id = "base_link"
+        publishing_data.header.stamp = rospy.Time.now()
 
-        pass
+        tmp_data = Point32()
+
+        for i in range(-1,2) :
+            for j in range(20) : 
+                
+                tmp_data.x = i
+                tmp_data.y = j
+                tmp_data.z = 0
+
+                publishing_data.polygon.points.append(tmp_data)
+        rospy.loginfo(publishing_data.polygon.points[0])
+        rospy.loginfo(publishing_data.polygon.points[1])
+        rospy.loginfo(publishing_data.polygon.points[2])
+        self.publisher1.publish(publishing_data)
+
 
 if __name__ == "__main__" :
-    turtle_mover()
+    topic_publisher()
