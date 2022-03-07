a39508a940327204c18e7989a4da462f5d14d6ba
seunggeon <tmdrjs9555@naver.com>
2022-03-07 11:06:23 +0900

    slam tutorial

---
+++ build/mobile_robot_gmapping/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kw-cobot/catkin_ws/devel/.private/mobile_robot_gmapping:$CMAKE_PREFIX_PATH"
+export PWD='/home/kw-cobot/catkin_ws/build/mobile_robot_gmapping'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/kw-cobot/catkin_ws/devel/.private/mobile_robot_gmapping/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/kw-cobot/catkin_ws/src/mobile_robot_gmapping:$ROS_PACKAGE_PATH"
---
+++ devel/share/mobile_robot_gmapping/cmake/mobile_robot_gmappingConfig.cmake
@@ -0,0 +1,1 @@
+/home/kw-cobot/catkin_ws/devel/.private/mobile_robot_gmapping/share/mobile_robot_gmapping/cmake/mobile_robot_gmappingConfig.cmake
